26a61d6f155a670a69fd2bb6406fbaccd71b2d7b
Pardeep Kumar <162547409+lazyhacker24@users.noreply.github.com>
2025-11-24 10:59:22 +0530

    portscan.py

--- modules/portscan.py
+++ modules/portscan.py
@@ -1,1 +1,18 @@
+import nmap
+from colorama import Fore
 
+def port_scan(target):
+    print(Fore.YELLOW + "\n[+] Running Port Scan...\n")
+
+    nm = nmap.PortScanner()
+    nm.scan(target, '1-500')
+
+    open_ports = []
+
+    for port in nm[target]['tcp']:
+        if nm[target]['tcp'][port]['state'] == 'open':
+            print(Fore.GREEN + f"[OPEN] {port}")
+            open_ports.append(port)
+
+    print(Fore.CYAN + f"\n[✔] Port Scan Finished! ({len(open_ports)} open)")
+    return open_ports
