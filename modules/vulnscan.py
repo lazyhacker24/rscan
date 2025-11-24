82d6f8c87042f2bb30f65cd6dadede80c362a41e
Pardeep Kumar <162547409+lazyhacker24@users.noreply.github.com>
2025-11-24 10:59:40 +0530

    vulnscan.py

--- modules/vulnscan.py
+++ modules/vulnscan.py
@@ -1,1 +1,13 @@
+from colorama import Fore
 
+def vuln_scan(target):
+    print(Fore.YELLOW + "\n[+] Running Vulnerability Scanner...\n")
+
+    vulns = []
+
+    if target.startswith("http://"):
+        print(Fore.RED + "[!] Website not using HTTPS (Security Risk)")
+        vulns.append("No HTTPS")
+
+    print(Fore.CYAN + f"\n[✔] Vulnerability Check Complete ({len(vulns)} alerts)")
+    return vulns
