ec426932db241513b4cd838acad62b75060186e7
Pardeep Kumar <162547409+lazyhacker24@users.noreply.github.com>
2025-11-24 11:00:07 +0530

    report.py

--- modules/report.py
+++ modules/report.py
@@ -1,1 +1,13 @@
+import os
 
+def save_report(data):
+    if not os.path.exists("output"):
+        os.mkdir("output")
+
+    with open("output/report.txt", "w") as file:
+        for section, result in data.items():
+            file.write(f"\n=== {section} ===\n")
+            for item in result:
+                file.write(str(item) + "\n")
+
+    print("\n[✔] Report saved at: output/report.txt")
