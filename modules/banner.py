c9042562071fb1197d7e22911dc0571a5b469536
Pardeep Kumar <162547409+lazyhacker24@users.noreply.github.com>
2025-11-24 10:57:29 +0530

    banner.py

--- modules/banner.py
+++ modules/banner.py
@@ -1,1 +1,14 @@
+from colorama import Fore, Style
 
+def banner():
+    print(Fore.CYAN + r"""
+ ███████████████████████████████
+ █▀█ █▀ █▀ █▀▄ ▄▀█ █▄█ █▄▄ █▄▀
+ █▄█ ▄█ ▄█ █▄▀ █▀█ ░█░ █▄█ █░█
+
+            R S C A N
+
+  Custom Offensive Recon & Vulnerability Scanner
+  Author: Pardeep (Raj) | Version: 1.0
+ ███████████████████████████████
+    """ + Style.RESET_ALL)
