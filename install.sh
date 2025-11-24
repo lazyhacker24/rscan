8bd75ea06751bf5f734557109b9a521581233fb1
Pardeep Kumar <162547409+lazyhacker24@users.noreply.github.com>
2025-11-24 10:52:36 +0530

    install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+echo "Installing RSCAN dependencies..."
+
+sudo apt update && sudo apt install -y python3 python3-pip nmap git
+
+pip3 install -r requirements.txt
+
+echo "Installation completed!"
+echo "Run tool: python3 rscan.py"
