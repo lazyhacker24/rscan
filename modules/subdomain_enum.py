1d0c3749c24a885ced41b862d25d535ee99d59fe
Pardeep Kumar <162547409+lazyhacker24@users.noreply.github.com>
2025-11-24 10:58:46 +0530

    subdomain_enum.py

--- modules/subdomain_enum.py
+++ modules/subdomain_enum.py
@@ -1,1 +1,21 @@
+import requests
+from colorama import Fore
 
+def subdomain_scan(target):
+    print(Fore.YELLOW + "\n[+] Running Subdomain Enumeration...\n")
+
+    wordlist = "wordlists/top.txt"
+    found = []
+
+    with open(wordlist, "r") as wl:
+        for line in wl:
+            sub = line.strip() + "." + target
+            try:
+                requests.get("http://" + sub, timeout=2)
+                print(Fore.GREEN + "[FOUND] " + sub)
+                found.append(sub)
+            except:
+                pass
+
+    print(Fore.CYAN + f"\n[✔] Subdomain Scan Complete! ({len(found)} found)")
+    return found
